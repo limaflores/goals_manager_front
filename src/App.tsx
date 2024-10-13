572008d4c169abcf2f5bf7f20481d54c707531b0
Felipe <limaflores@gmail.com>
2024-10-13 09:15:28 -0300

    feat: add styling to button

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/target.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import { Plus } from 'lucide-react'
+import logo from './assets/logo-in.svg'
+import letsStart from './assets/lets-start-illustration.png'
+import { Button } from './components/ui/button'
+export function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+    <div className="h-screen flex flex-col items-center justify-center gap-8">
+      <img src={logo} alt="logo.in" style={{ width: '5%', height: '10%' }} />
+      <img
+        src={letsStart}
+        alt="logo.in"
+        style={{ width: '15%', height: '25%' }}
+      />
+      <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
+        Você ainda não cadastrou nenhuma meta, que tal cadastrar um agora mesmo?
       </p>
-    </>
+
+      <Button>
+        <Plus className="size-4" />
+        Cadastar meta
+      </Button>
+
+      {/* <button
+        type="button"
+        className="px-4 py-2.5 rounded-lg  bg-violet-500 text-violet-50 flex items-center gap-2 text-sm font-medium tracking-tight hover:bg-violet-600"
+      >
+        <Plus className="size-4" />
+        Cadastar meta
+      </button> */}
+    </div>
   )
 }
-
-export default App
